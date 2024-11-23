eacec96eac48c48d5896cc49c353030d69715253
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-11-23 08:57:20 +0530

    get brands

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -15,7 +15,18 @@ const createBrand = async (req, res) => {
     }
 };
 
+//get all brands
+const getBrands = async (req, res) => {
+    try {
+        const brands = await Brand.find();
+        res.status(200).json(brands);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting brands", error: error.message });
+    }
+};
+
 module.exports = {
-    createBrand
+    createBrand,
+    getBrands
 
 }
