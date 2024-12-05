a1cb740fdc92f47f3e6fd4d9a79b86096a1e4417
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-12-05 17:53:09 +0530

    test f1

--- routers/brandRoutes.js
+++ routers/brandRoutes.js
@@ -5,17 +5,7 @@ const cors = require('cors');
 
 // Updated CORS configuration
 router.use(
-    cors({
-        credentials: true,
-        origin: (origin, callback) => {
-            const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-            if (allowedOrigins.includes(origin) || !origin) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        }
-    })
+    cors()
 );
 
 const { createBrand, getBrands, deleteBrand, updateBrand } = require('../controllers/brandController')
